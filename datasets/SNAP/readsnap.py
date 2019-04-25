96e5930b1d339dcef02e525253c3c68e06954616
Saurabh Sawlani <saurabh.sawlani@gmail.com>
2019-04-25 18:19:06 -0400

    fixed datasets

--- datasets/SNAP/readsnap.py
+++ datasets/SNAP/readsnap.py
@@ -8,6 +8,8 @@ for file in os.listdir(os.getcwd()):
     if (ext == '.snap'):
         f=open(filename + ".txt", "w+")
         G = nx.read_edgelist(file)
+        mapping=dict(zip(G.nodes(),range(G.number_of_nodes())))
+        G=nx.relabel_nodes(G,mapping)
         f.write(str(G.number_of_nodes())+" "+str(G.number_of_edges())+"\n")
         for e in G.edges:
             f.write(str(int(e[0])+1)+" "+str(int(e[1])+1)+"\n")
