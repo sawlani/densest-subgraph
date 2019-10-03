bf25d1dcf5354da3915353993c0fd2021e0df59d
Saurabh Sawlani <saurabh.sawlani@gmail.com>
2019-10-02 20:12:33 -0400

    fixed load adjust code

--- load_adjust.cpp
+++ load_adjust.cpp
@@ -4,41 +4,42 @@
 
 using namespace std;
 
-int main() {
-	
-	auto start = chrono::steady_clock::now();
+int main(int argc, char** argv) {
+
+  	auto start = chrono::steady_clock::now();
 
 	int n, m;
-	double eps = 0.1;
+	double eps = atof(argv[1]);
 
 	cin >> n >> m;
-	cout << n << m;
+	
 	double * r = new double[m];
     memset(r, 0, sizeof(double) * m);
   	
   	double * load = new double[n];
     memset(load, 0, sizeof(double) * n);
   	
-  	int ** e = new int[m][2];
-  	memset(e, 0, sizeof(double) * m*2);
+  	int * e_left = new int[m];
+  	memset(e_left, 0, sizeof(int) * m);
+  	
+  	int * e_right = new int[m];
+  	memset(e_right, 0, sizeof(int) * m);
   	
 	for (int i = 0; i < n+1; i++)
 		load[i] = 0; // loads on vertices
 	for (int i = 0; i < m; i++) {
 		int p, q;
 		cin >> p >> q;
-		e[i][0] = p; // left end point of edge i is p
-		e[i][1] = q; // right end point of edge i is q
+		e_left[i] = p; // left end point of edge i is p
+		e_right[i] = q; // right end point of edge i is q
 		
-		if (load[p] < load[q]-1)
+		if (load[p] < load[q])
 			r[i] = 0; // how much of the edge is assigned to the right side (here it is assigned to the left because p's load is lower)
-		else if (load[q] < load[p]-1)
-			r[i] = 1; // how much of the edge is assigned to the right side (here it is assigned to the right because q's load is lower)
 		else
-			r[i] = load[p]/2-load[q]/2 + 0.5;
-
+			r[i] = 1; // how much of the edge is assigned to the right side (here it is assigned to the right because q's load is lower)
+		
 		load[q] += r[i];
-		load[p] += 1 - r[i];
+		load[p] += (1 - r[i]);
 	}
 
 	//for (int i = 0; i < n+1; i++)
@@ -46,16 +47,16 @@ int main() {
 	//cout << endl << endl;
 	
 	auto mid = chrono::steady_clock::now();
-
+	cerr << "Time for reading input: " << chrono::duration_cast<chrono::milliseconds>(mid - start).count() << " ms" << endl;
+  	
 	bool flag = false;
 		
 	int ii = 0;
 	while (!flag) {
 		ii += 1;
-		cout << ii;
 		flag = true;
 		for (int i = 0; i < m; i++) {
-			int p = e[i][0], q = e[i][1];
+			int p = e_left[i], q = e_right[i];
 			while (load[p]<load[q]-eps and r[i]>=eps) { // flip eps from right to left (load at p is smaller than that at q)
 				flag = false;
 				load[p] += eps;
@@ -72,8 +73,7 @@ int main() {
 	}
 
 	auto end = chrono::steady_clock::now();
-	cerr << "Time for reading input: " << chrono::duration_cast<chrono::milliseconds>(mid - start).count() << " ms" << endl;
-  	cerr << "Time for finding DSP value: " << chrono::duration_cast<chrono::milliseconds>(end - mid).count() << " ms" << endl;
+	cerr << "Time for finding DSP value: " << chrono::duration_cast<chrono::milliseconds>(end - mid).count() << " ms" << endl;
 
 	//for (int i = 0; i < n+1; i++)
 	//	cout << load[i] << endl;
