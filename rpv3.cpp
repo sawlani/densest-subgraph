213774e08bd22e3776915b5600df38d956038aaa
Richard Peng <richard.peng@gmail.com>
2019-10-03 02:52:29 +0100

    faster peeling code using CSR

--- rpv3.cpp
+++ rpv3.cpp
@@ -13,6 +13,7 @@
 #include <unordered_set>
 #include <algorithm>
 #include <numeric>
+#include <iomanip>
 using namespace std;
 
 
@@ -47,31 +48,6 @@ inline int getInt() {
 	}
 	return res;
 }
-Node * lists;
-
-__inline void linklists (int x, int y) {
-	if(y == 0) return;
-	lists[x].next = y;
-	lists[y].prev = x;
-};
-int * nxt, * prv, *itr;
-__inline void linknodes (int x, int y) {
-	if(y == -1) return;
-	nxt[x] = y;
-	prv[y] = x;
-};
-__inline void eraselist(int x) {
-	lists[lists[x].prev].next = lists[x].next;
-	if(lists[x].next != 0) lists[lists[x].next].prev = lists[x].prev;
-};
-__inline void erasenode (int x) {
-	if(prv[x] == -1) {
-		lists[itr[x]].idx = nxt[x];
-	}
-	if(prv[x] != -1) nxt[prv[x]] = nxt[x];
-	if(nxt[x] != -1) prv[nxt[x]] = prv[x];
-
-};
 int l = 0;
 Edge * edges;
 int * idx;
@@ -80,13 +56,20 @@ __inline void build(int x, int y) {
 		edges[l].y = y;
 		idx[x] = l;
 };
-	
+int * id1;
+int l1 = 0;
+Edge * e1;
+__inline void bd1(int x, int y) {
+	e1[++l1].next = id1[x];
+	e1[l1].y = y;
+	id1[x] = l1;
+}
 int main(int argc, char** argv) {
 
+  
 	int iters = atoi(argv[1]);
 	//double eps = 1.0/iters;
 	string input_file = argv[2];
-
 	string output_file;
 	ofstream outfile;
 
@@ -100,32 +83,34 @@ int main(int argc, char** argv) {
 	}
 	//string st1 ("tests/"+ss);
 	//string st2 ("tests/"+ss+".a");
-
+	
 	freopen (input_file.c_str(), "r", stdin); //input file
 	//freopen (output_file.c_str(), "a", stdout); //output file
 	outfile.open(output_file.c_str(), ios_base::app);
+	auto begin = clock();
 	n = getInt(); m = getInt();
 	edges = new Edge[m * 2 + 10];
+	e1 = new Edge[n + m + 10];
 	idx = new int[n];
+	id1 = new int[n];
+	memset(id1, 0, sizeof(int) * n);
 	memset(idx, 0, sizeof(int) * n);
 	int * init_deg = new int[n];
 	memset(init_deg, 0, sizeof(int) * n);
 	l = 0;
 	//int LIM = 2 * m;
-
-	lists = new Node[n + 2 * m + 10];
 	int n_list = 0;
-	itr = new int[n];
 	int * deg = new int[n], * w = new int[n], * pos = new int [n];
 	memset(deg, 0, sizeof(int) * n);
 	memset(w, 0, sizeof(int) * n);//initial vertex weights=0, i.e., no self loops at the start 
 	memset(pos, 0, sizeof(int) * n);
-	prv = new int[n]; nxt = new int[n];
-	cout << "MEM DECLARED: " << clock() << endl;
+	//cout << clock() << endl;
+	//outfile << clock() << endl;
 	for (int i = 0; i < m; i++) {
 		int p, q;
 		p = getInt();
 		q = getInt();
+		assert(1 <= p && p <= n && 1 <= q && q <= n);
 		p -= 1;
 		q -= 1;
 		build(p, q);
@@ -133,106 +118,59 @@ int main(int argc, char** argv) {
 		init_deg[p]++;
 		init_deg[q]++;
 	}
-	cout << "IO DONE: " << clock() << endl;
+	//cout << clock() << endl;
+	outfile << "Test dataset: " << input_file << endl;
+	outfile << "I/O finishes: " << float(clock()-begin) /  CLOCKS_PER_SEC << endl;
+	cerr << "I/O finishes: " << float(clock()-begin) /  CLOCKS_PER_SEC << endl;
 	vector<int> m_ans;
 	double mm_density = 0;
-	int sizeofcnt = 2 * iters * n + 10;
-	int * cnt = new int[sizeofcnt],  * o = new int[n], * nwo = new int[n];
+	double gr_density = 0;
 	for (int tt = 0; tt < iters; tt++) {
 		//cout << tt << endl;
-		memset(cnt, 0, sizeof(int) * sizeofcnt);
 		//cout << clock() << endl;
+		int mx = 0;
+
+		memset(id1, 0, sizeof(int) * n);
+		l1 = 0;
 		for (int i = 0; i < n; i++) {
-			nxt[i] = prv[i] = -1;
 			pos[i] = 0;
 			//cout << w[i] << ' ' << init_deg[i] << endl;
 			deg[i] = w[i] + init_deg[i]; //degree for this iteration is "vertex weight" + actual degree
-			o[i] = i;
-			cnt[deg[i]]++;
+			mx = max(mx, deg[i]);
+			bd1(deg[i], i);
 		}
-		{
-			for(int i = 1; i < sizeofcnt; i++) cnt[i] = cnt[i - 1] + cnt[i];
-			for(int i = 0; i < n; i++) o[--cnt[deg[i]]] = i;		
-			/*memset(cnt, 0, sizeof(int) * sizeofcnt);
-			for(int i = 0; i < n; i++) {
-				cnt[deg[i] / (n * 2)]++;
-			}
-			for(int i = 1; i < sizeofcnt; i++) cnt[i] = cnt[i - 1] + cnt[i];
-			for(int i = n - 1; i >= 0; i--) nwo[--cnt[deg[o[i]] / (n * 2)]] = o[i];
-			for(int i = 0; i < n; i++) o[i] = nwo[i];*/
-		}
-		n_list = 0;
-		//cout << clock() << '?' << endl;
-
-		for(int i = 0; i < n; i++) {
-			int v = o[i];
-
-			if(n_list == 0 || lists[n_list].deg != deg[v]) {
-				++n_list;
-				lists[n_list].clear();
-				linklists(n_list - 1, n_list);
-				lists[n_list].deg = deg[v];
-			}
-			//printf("%d %d %d %d %d\n", v, lists[n_list].idx, deg[v], w[v], init_deg[v]);
-			linknodes(v, lists[n_list].idx);
-			lists[n_list].idx = v;
-			itr[v] = n_list;
-		}
-		//for(int i = 0; i < n; i++) printf("prv[%d] = %d\n", i, nxt[i]);
-		//exit(0);
 		double max_density = (double)m / n;
 		int cur_m = m, cur_n = n;
 		vector<int> ans;
 		int max_size = 0;
-		while(lists[0].next) {
-			//cout << "??" << endl;
-			int i = lists[0].next;
-			int k = lists[i].idx;
-			//cout << "k = " << k << ' ' << i << ' ' << lists[i].deg << '!' << deg[k] << ' ' << nxt[k] << endl;
-			if(nxt[k] == -1) {
-				eraselist(i);
-			}else {
-				erasenode(k);
-			}
-			pos[k] = -1;
-			w[k] = deg[k]; //increment vertex weight for the next iteration (self loops)
-			cur_n -= 1;
-			ans.push_back(k);
-			for (int p = idx[k]; p; p = edges[p].next) { //decrement degrees of k's neighbors
-				int j = edges[p].y;
-				if(pos[j] == -1) continue;
-				cur_m -= 1;
-				//cout << k << "->" << j << ' ' << deg[j] << ' ' << pos[j] << endl;
-				//cout << "?" << ordered_deg[deg[j]].size()<< endl;
-				int i = itr[j];
-				erasenode(j);
-				int i1 = lists[i].prev;
-				//cout << j << ' ' << deg[j] << ' ' << i << 'j' << lists[i].deg << endl;
-				//cout << i1 << ' ' << lists[i1].deg << endl;
-				if(lists[i].idx == -1) eraselist(i);
-				deg[j]--;				
-				prv[j] = nxt[j] = -1;
-				if(i1 == 0 || lists[i1].deg != deg[j]) {
-					++n_list;
-					lists[n_list].clear();
-					itr[j] = n_list;
-					int i2 = lists[i1].next;
-					lists[n_list].deg = deg[j];
-					lists[n_list].idx = j;
-					linklists(i1, n_list);
-					if(i2) linklists(n_list, i2);
-				}else {
-					linknodes(j, lists[i1].idx);
-					lists[i1].idx = j;
-					itr[j] = i1;
+		for(int i(0); i <= mx; i++) {
+			//printf("%d\n", i);
+			int bak = id1[i]; id1[i] = 0;
+			int mn = i + 1;
+			for(int u = bak; u; u = e1[u].next) {
+				int v = e1[u].y;
+				if(pos[v] == -1) continue;
+				//if(i == 1) cout << v << endl;
+				ans.push_back(v);
+				w[v] = deg[v];
+				pos[v] = -1;
+				cur_n -= 1;
+				for(int p(idx[v]); p; p = edges[p].next) {
+					int j = edges[p].y;
+					if(pos[j] == -1) continue;
+					cur_m -= 1;
+					deg[j]--;
+					mn = min(mn, deg[j]);
+					bd1(deg[j], j);
 				}
-				//cout << itr[j]->first << ' ' << deg[j] << ' ' << itr[j]->second[pos[j]] << ' ' << j << endl;
 			}
+			//printf("%d\n", i);
 			if(cur_n == 0) continue;
 			if(max_density < (double)cur_m / cur_n) {
 				max_size = ans.size();
 			}
 			max_density = max(max_density, (double)cur_m / cur_n);
+			i = min(i, mn - 1);
 		}
 		reverse(ans.begin(), ans.end());
 		ans.resize(n - max_size);
@@ -240,21 +178,29 @@ int main(int argc, char** argv) {
 			m_ans = ans;
 			mm_density = max_density;
 		}
-		cout << "Max density = " << max_density << " (iteration " << tt << ")" << endl;
+		if (tt==0) gr_density = mm_density;
+		outfile <<  tt << ", " << std::setprecision(12) << mm_density << endl;
+		//		printf("Max density = %.12f (iteration %d)\n", mm_density, tt);
+		outfile << "Time: " << float(clock()-begin) /  CLOCKS_PER_SEC << endl;
+		cerr <<  tt << ", " << std::setprecision(12) << mm_density << endl;
+		//		printf("Max density = %.12f (iteration %d)\n", mm_density, tt);
+		cerr << "Time: " << float(clock()-begin) /  CLOCKS_PER_SEC << endl;
+	
 	}
-
-	//outfile << "Time: " << clock() << endl;
+	cerr << "Improvement over greedy: " << float((mm_density - gr_density)*100/gr_density) << "%" << endl;
+		outfile << "Improvement over greedy: " << float((mm_density - gr_density)/gr_density) << endl;
 	cerr << "Time: " << clock() << endl;
+
 	//outfile << "Filename: " << input_file << endl;
 	//outfile << "Iterations: " << iters << endl;
 	//outfile << "Max load: " << w[V[0]] * eps << endl;
-	cerr << "Approximate maximum density: " << mm_density << endl;
-	cerr << "Approximate densest subgraph:" << endl;
-	outfile << m_ans.size() << endl;
-	for(int i : m_ans) 
-	{
-		outfile << i << endl;
-	}
+	//cerr << "Approximate maximum density: " << mm_density << endl;
+	//cerr << "Approximate densest subgraph:" << endl;
+	//outfile << m_ans.size() << endl;
+	//for(int i : m_ans) 
+	//{
+	//	outfile << i << endl;
+	//}
 	////////////////////////
 	// If the solution is known, compute actual value of maximum density from solution file:
 	////////////////////////
