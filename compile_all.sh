58e2f4ff76faabcb5ddfde904c9f3ebf36851d80
Saurabh Sawlani <saurabh.sawlani@gmail.com>
2019-10-11 13:06:18 -0400

    updated shell scripts

--- compile_all.sh
+++ compile_all.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 g++ -O3 iterative_peeling.cpp -o ip
+g++ -O3 iterative_peeling_naive_weighted.cpp -o ipnw
 cd exactDSP-cpp
 gcc -O3 -Wall -DNO_MAIN hi_pr.c -c -o hi_pr.o
 g++ -O3 densest.cpp hi_pr.o -o ../exact
+g++ -O3 densest_weighted.cpp hi_pr.o -o ../exactweighted
+cd ..
--- create_triangle_weighted.sh
+++ create_triangle_weighted.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 ./tools/mace22/mace Ce -l 3 -u 3 $1 $1-triangles
 python tools/triangle-weights.py $1 $1-triangles $1-triangle-weighted
+./ipnw 10 < $1-triangle-weighted
+rm -f $1-triangles $1-triangle-weighted
